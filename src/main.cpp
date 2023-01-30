3fb57f9d23316a55b111fd2e43b647baed174b83
Alejandro Pulido Saravia <pulidosaravia@gmail.com>
2023-01-30 14:59:07 -0500

    Deep Sleep Mode integrated

--- src/main.cpp
+++ src/main.cpp
@@ -33,11 +33,14 @@ unsigned long lastTime = 0;
 unsigned long timerDelay = 3;
 
 int pinShutdown = 14;
+int sleepPin = 12;
 
 bool deviceConnected = false;
+bool goSleep = false;
 
 // create timer
 hw_timer_t *timer = NULL;
+hw_timer_t *timer1 = NULL;
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
@@ -61,7 +64,7 @@ class MyServerCallbacks : public BLEServerCallbacks
   void onConnect(BLEServer *pServer)
   {
     digitalWrite(pinShutdown, HIGH);
-    Serial.println("BLE Client connected");
+    //Serial.println("BLE Client connected");
     dataReady = false;
     dataRead = 0;
     timerAlarmEnable(timer);
@@ -71,16 +74,17 @@ class MyServerCallbacks : public BLEServerCallbacks
   void onDisconnect(BLEServer *pServer)
   {
     digitalWrite(pinShutdown, LOW);
-    Serial.println("BLE Client disconnected");
+    //Serial.println("BLE Client disconnected");
     timerAlarmDisable(timer);
+    // start the timer to go to sleep after 30 seconds
+    timerAlarmWrite(timer1, 30000000, false);
+    timerAlarmEnable(timer1);
     dataReady = false;
     dataRead = 0;
     deviceConnected = false;
     pServer->getAdvertising()->start();
   }
 };
-
-
 
 void IRAM_ATTR onTimer()
 {
@@ -89,35 +93,55 @@ void IRAM_ATTR onTimer()
   uint16_t buffer[1] = {0};
   i2s_read(I2S_NUM_0, &buffer, sizeof(buffer), &bytes_read, 1);
   sendData();*/
-  
+
   data[countBuffer][countRow] = adc1_get_raw(ADC_INPUT);
   countRow++;
-  if(countRow == bufferLength){
+  if (countRow == bufferLength)
+  {
     countRow = 0;
     dataRead = countBuffer;
     countBuffer++;
     dataReady = true;
-    //dataRead =0;
-    if(countBuffer == bufferCount){
+    // dataRead =0;
+    if (countBuffer == bufferCount)
+    {
       countBuffer = 0;
-      //dataRead=1;
+      // dataRead=1;
     }
   }
 
   // Serial.println(millis());
 }
 
+void IRAM_ATTR goToSleep()
+{
+  goSleep = true;
+}
+
 void setup()
 {
+
   pinMode(pinShutdown, OUTPUT);
-  digitalWrite(pinShutdown, LOW);
+
+  digitalWrite(pinShutdown, HIGH);
+
+  // set up the sleep pin to wake up the device from sleep
+  pinMode(sleepPin, INPUT);
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_12, 1);
+
+  // set up the timer to go to sleep after 30 seconds
+  timer1 = timerBegin(1, 80, true);
+  timerAttachInterrupt(timer1, &goToSleep, true);
+  timerAlarmWrite(timer1, 30000000, false);
+  timerAlarmEnable(timer1);
+
   Serial.begin(115200);
 
   // Create the BLE Device
   BLEDevice::init(bleServerName);
 
   // set the mtu to 512
-  //BLEDevice::setMTU(512);
+  // BLEDevice::setMTU(512);
 
   // Create the BLE Server
   BLEServer *pServer = BLEDevice::createServer();
@@ -153,7 +177,6 @@ void setup()
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &onTimer, true);
   timerAlarmWrite(timer, 4000, true);
-  
 }
 
 // create varianble to save the millis of the last time the data was sent
@@ -161,21 +184,32 @@ unsigned long lastTimeDataSent = 0;
 
 void loop()
 {
-  if(dataReady){
+  if (goSleep)
+  {
+    if (!deviceConnected)
+    {
+      //Serial.println("Going to sleep");
+      esp_sleep_enable_ext0_wakeup(GPIO_NUM_12, 1);
+      esp_deep_sleep_start();
+    }
+    goSleep = false;
+  }
+  if (dataReady)
+  {
     lastTimeDataSent = millis();
     dataReady = false;
     Serial.printf("timer interrupt %d \t", lastTimeDataSent);
     sendData();
     Serial.printf("timer end interrupt %d \n", millis());
-    //data[count] = dataRead;
-    //count++;
-    
-    //Serial.println(lastTimeDataSent);
+    // data[count] = dataRead;
+    // count++;
+
+    // Serial.println(lastTimeDataSent);
     /*if(count == 2){
       count = 0;
       sendData();
     }*/
-    //sendData(dataRead);
+    // sendData(dataRead);
   }
   /*data = analogRead(32);
   data = float(data * (3.3 / 4095.0));
@@ -226,30 +260,30 @@ void loop()
 
 void sendData()
 {
-  
+
   if (deviceConnected)
   {
-    //Serial.println(millis());
-    // ecgMonitor.notify();
     // Serial.println(millis());
-    // data = analogRead(32);
+    //  ecgMonitor.notify();
+    //  Serial.println(millis());
+    //  data = analogRead(32);
 
     // get the voltage reading
-    //data = float(dataIn * (3.3 / 4095.0));
+    // data = float(dataIn * (3.3 / 4095.0));
     // remove the offset voltage
     // data = data > 1 ? data - 1.65 : data;
-    //data = float(data - float(1.65));
+    // data = float(data - float(1.65));
 
-    //data = float(data / 1.1);
-    // convert float to string with 18 decimal places
-    //char str[4];
-    //char str2[4];
-    //char str3[9];
-    // convert int to string
-    //intToChar(data[0], str, 4);
-    //intToChar(data[1], str2, 4);
-    
-    String dataString = joinData(data[dataRead],bufferLength);
+    // data = float(data / 1.1);
+    //  convert float to string with 18 decimal places
+    // char str[4];
+    // char str2[4];
+    // char str3[9];
+    //  convert int to string
+    // intToChar(data[0], str, 4);
+    // intToChar(data[1], str2, 4);
+
+    String dataString = joinData(data[dataRead], bufferLength);
 
     /*str3[6] = str2[0];
     str3[7] = str2[1];
@@ -257,25 +291,24 @@ void sendData()
     str3[9] = str2[3];
     str3[10] = str2[4];*/
 
-
-    //dtostrf(data[0], -21, 18, str);
-    //dtostrf(data[1], -21, 18, str);
+    // dtostrf(data[0], -21, 18, str);
+    // dtostrf(data[1], -21, 18, str);
 
     // Serial.println(str);
     // unir los dos datos
-    //strcat(str, ",");
-    //strcat(str, str2);
-    //Serial.printf("%s \n", str3);
+    // strcat(str, ",");
+    // strcat(str, str2);
+    // Serial.printf("%s \n", str3);
     ecgMonitor.setValue(dataString.c_str());
-    //ecgMonitor.setValue(str);
-    // String dataString = String(data);
-    // ecgMonitor.setValue(dataString.c_str());
-    //  set battery leve in 100 %
-    // batteryLevel.setValue("200");
-    // batteryLevel.setValue(String(0x64).c_str());
+    // ecgMonitor.setValue(str);
+    //  String dataString = String(data);
+    //  ecgMonitor.setValue(dataString.c_str());
+    //   set battery leve in 100 %
+    //  batteryLevel.setValue("200");
+    //  batteryLevel.setValue(String(0x64).c_str());
 
     ecgMonitor.notify();
-    //Serial.println(millis());
+    // Serial.println(millis());
   }
 }
 
@@ -283,7 +316,7 @@ void intToChar(int data, char *str, int size)
 {
   int i = 0;
 
-  while (i<size)
+  while (i < size)
   {
     str[i] = (data % 10) + '0';
     data = data / 10;
@@ -296,7 +329,6 @@ void intToChar(int data, char *str, int size)
     str[j] = str[i - j - 1];
     str[i - j - 1] = temp;
   }
-
 }
 
 // create join function to join an array of char
@@ -305,7 +337,7 @@ String joinData(int vals[bufferLength], int size)
   String str = "";
   for (int i = 0; i < size; i++)
   {
-    str += String(vals[i])+",";
+    str += String(vals[i]) + ",";
   }
   return str;
 }
